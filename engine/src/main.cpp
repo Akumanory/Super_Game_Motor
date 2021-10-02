d0763653ec5fa6e78534fe39e5e80fdab3452cf2
xmmt <mkmax472@gmail.com>
2021-10-02 14:37:42 +0300

    old_main

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -15,39 +15,6 @@
 #include <format>
 #include <iostream>
 
-// old main
-/*
-#include "DirectXIncludes.h"
-#include "Logs.h";
-#include "Framework.h"
-*/
-
-/*
-int WINAPI main(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
-{
-	Logs::Debug("Main call"); // Тестовый лог
-
-	Framework framework; 
-	
-	if (framework.Initialize(hInstance, "Test window", 1600, 900))
-	{
-		Logs::Debug("Start ProcessMessage"); // Тестовый лог
-		while (framework.ProcessMessages())
-		{
-			framework.Update();
-			framework.RenderFrame();
-		}
-		Logs::Debug("End ProcessMessage"); // Тестовый лог
-	}
-
-	return 0;
-};
-*/
-
 using namespace std;
 using namespace motor;
 
---
+++ engine/src/old_main.cpp
@@ -0,0 +1,24 @@
+#include <motor/other/DirectXIncludes.h>
+#include <motor/other/Logs.h>
+#include <motor/other/Framework.h>
+
+int WINAPI main(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR lpCmdLine,
+  int nCmdShow) {
+    Logs::Debug("Main call"); // Тестовый лог
+
+    Framework framework;
+
+    if (framework.Initialize(hInstance, "Test window", 1600, 900)) {
+        Logs::Debug("Start ProcessMessage"); // Тестовый лог
+        while (framework.ProcessMessages()) {
+            framework.Update();
+            framework.RenderFrame();
+        }
+        Logs::Debug("End ProcessMessage"); // Тестовый лог
+    }
+
+    return 0;
+};

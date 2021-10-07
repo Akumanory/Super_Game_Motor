99d78377765019bef81455f93a10bd51827ea0d2
xmmt <mkmax472@gmail.com>
2021-10-08 00:39:04 +0300

    added entt

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -5,6 +5,8 @@
 #include <imgui_impl_win32.h>
 #include <imgui_impl_dx11.h>
 
+#include <entt/entt.hpp>
+
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
 
@@ -62,6 +64,49 @@ int WINAPI WinMain(
         demoOpened = true
         luaOpened = true
     )lua");
+
+    entt::registry registry;
+
+    struct position {
+        float x;
+        float y;
+    };
+
+    struct velocity {
+        float dx;
+        float dy;
+    };
+
+    for (auto i = 0u; i < 10u; ++i) {
+        const auto entity = registry.create();
+        registry.emplace<position>(entity, i * 1.f, i * 1.f);
+        if (i % 2 == 0) {
+            registry.emplace<velocity>(entity, i * .1f, i * .1f);
+        }
+    }
+
+    auto update = [](entt::registry& registry) {
+        auto view = registry.view<const position, velocity>();
+
+        // use a callback
+        view.each([](const auto& pos, auto& vel) { /* ... */ });
+
+        // use an extended callback
+        view.each([](const auto entity, const auto& pos, auto& vel) { /* ... */ });
+
+        // use a range-for
+        for (auto [entity, pos, vel] : view.each()) {
+            // ...
+        }
+
+        // use forward iterators and get only the components of interest
+        for (auto entity : view) {
+            auto& vel = view.get<velocity>(entity);
+            // ...
+        }
+    };
+
+    update(registry);
 
     // Create application window
     //ImGui_ImplWin32_EnableDpiAwareness();
